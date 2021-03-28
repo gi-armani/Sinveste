f6876f81fc86e396e12a752761d26080e1e3456b
gi-armani <giovana_armani@usp.br>
2021-03-28 00:44:25 -0300

    Index final

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
     <body>
         <div id='footer'>
             <img src='fotos/logo.svg' class='logo'/>
-            <p>Totalemnte executado pelos Sintetizados, membros da Empresa Júnior de Sistemas de Informações da EACH-USP. Nós desenvolvemos sites, aplicativos mobiles, sistemas e design de interface. 
+            <p class="text-footer">Totalmente executado pelos Sintetizados, membros da Empresa Júnior de Sistemas de Informações da EACH-USP. Nós desenvolvemos sites, aplicativos mobiles, sistemas e design de interface. 
                 Para saber mais sobre a gente, acesse as seguintes redes sociais :) </p>
             <a href='https://www.linkedin.com/company/sintese-jr' target="blank"><img src='fotos/linkedinIcon.svg' class='icon'/></a>
             <a href='https://www.instagram.com/sintesejr/' target="blank"><img src='fotos/instaIcon.svg' class='icon'/></a>
