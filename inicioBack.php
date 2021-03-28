13d4c5f5c32ee5dc68ed2bb5fd60218a63f1db4b
gi-armani <giovana_armani@usp.br>
2021-03-28 17:09:28 -0300

    Sessions atualizadas

--- inicioBack.php
+++ inicioBack.php
@@ -9,29 +9,58 @@ if(!$conexao) {
 $destino = mysqli_real_escape_string($conexao, $_POST['destino']);
 $data = mysqli_real_escape_string($conexao, $_POST['data']);
 $orcamento = mysqli_real_escape_string($conexao, $_POST['orcamento']);
+//$id = mysqli_real_escape_string($conexao, $_POST['id']);
 
-//queries aqui
-$query;
+$destinoId;
+$query = "select id from destinos where nome='${destino}';";
 $resultado = mysqli_query($conexao, $query);
-$milhasTotais = mysql_fetch_assoc($resultado);
-echo $milhasTotais;
+if(mysqli_num_rows($resultado)){
+    $dados = mysqli_fetch_assoc($resultado);
+    $destinoId =  $dados["id"];
+}
 
-$query;
+$milhasTotais;
+$query = "select preco from viagens where destinoId=${destinoId};"; 
 $resultado = mysqli_query($conexao, $query);
-$voos = mysql_fetch_assoc($resultado);
-echo $voos;
+if(mysqli_num_rows($resultado)){
+    $dados = mysqli_fetch_assoc($resultado);
+    $milhasTotais =  $dados["preco"];
+}
 
-$query;
+$orcamento_atual;
+$query = "select orcamento_atual from viagens where destinoId={$destinoId};";
 $resultado = mysqli_query($conexao, $query);
-$hoteis = mysql_fetch_assoc($resultado);
-echo $hoteis;
+if(mysqli_num_rows($resultado)){
+    $dados = mysqli_fetch_assoc($resultado);    
+    $orcamento_atual = $dados["orcamento_atual"];
+}
 
+$voos;
+$query = "select * from voos where destinoId=${destinoId};"; 
+$resultado = mysqli_query($conexao, $query);
+if(mysqli_num_rows($resultado)){
+    $voos = mysqli_fetch_assoc($resultado);
+}
+
+$hoteis;
+$query = "select * from hoteis where localId=${destinoId}"; 
+$resultado = mysqli_query($conexao, $query);
+if(mysqli_num_rows($resultado)){
+    $hoteis = mysqli_fetch_assoc($resultado);
+}
+
+/*
+$query = "select vooId from viagens where destinoId=${destinoId}"; 
+$resultado = mysqli_query($conexao, $query);
+$hotelViagem = mysql_fetch_assoc($resultado);
+echo $hotelViagem;*/
 
 $_SESSION['destino'] = $destino;
 $_SESSION['milhasAcumuladas'] = $orcamento;
-$_SESSION['milhasTotais'] = $milhasTotais; //query
-$_SESSION['voos'] = $voos; //query
-$_SESSION['hoteis'] = $hoteis; //query
+$_SESSION['milhasTotais'] = $milhasTotais; 
+$_SESSION['voos'] = $voos;
+$_SESSION['hoteis'] = $hoteis; 
+
 
 header('Location: resultados.php');
 exit();
