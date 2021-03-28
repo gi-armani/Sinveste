41ab204b83210cdcabcbfc70a66227089551c2b9
Maria Eduarda Rodrigues Garcia <47001648+dudagarcia@users.noreply.github.com>
2021-03-28 01:28:00 -0300

    pagina de sugestoes

---
+++ sugestoes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href='sugestoes.css'/>
+    </head>
+    <body>
+        <?php include 'header.php';?>
+        <div class='container'>
+            <h1>Nossas sugestões para você:</h1>
+            <div class='image-div'>
+                <img src='fotos/sugestoes.svg'/>
+            </div>
+            
+            <div class='footer'>
+                <?php include 'footer.php';?>
+            </div>
+        </div>
+        
+
+    </body>
+</html>
