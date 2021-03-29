5b1cdd6a197d7957c7e6971e2d85007bb4f89cf9
gi-armani <giovana_armani@usp.br>
2021-03-28 21:19:25 -0300

    final

--- resultados.php
+++ resultados.php
@@ -249,7 +249,27 @@
                     <div id='minhas-reservas' style='display: none'>
                         <p class='title'> Voo </p>
                         
-                        <div class="box" style='margin-top:0%'>
+                        <?php
+                            while($row = mysqli_fetch_array($voos, MYSQLI_ASSOC)){
+                                $partida; 
+                                $destino;
+                                $partidaId = $row['partidaId'];
+                                $query = "select nome from destinos where id=${partidaId}";
+                                $resultado = mysqli_query($conexao, $query);
+                                if(mysqli_num_rows($resultado)){
+                                    $dados = mysqli_fetch_assoc($resultado);
+                                    $partida = $dados['nome'];
+                                }
+                                $destinoId = $row['destinoId'];
+                                $query = "select nome from destinos where id=${destinoId}";
+                                $resultado = mysqli_query($conexao, $query);
+                                if(mysqli_num_rows($resultado)){
+                                    $dados = mysqli_fetch_assoc($resultado);
+                                    $destino = $dados['nome'];
+                                }
+                        ?>
+                        <div class="box">
+                        <!-- <p class="title">Voo</p>  -->
                             <div class="wrapper">
                                 <div class="esquerda">
                                     <div>
@@ -257,22 +277,63 @@
                                         <p class="text"> Destino:<?php echo $destino?> </p>
                                     </div>
                                     <div>
-                                        <p class="text"> 02:10:00 </p>
-                                        <p class="text"> 10/01/2022 </p>
+                                        <p class="text"> <?php echo $row['duracao']?> </p>
+                                        <p class="text"> <?php echo trataData($row['data']) ?> </p>
                                     </div>
                                 </div>
                                 <div class="direita">
                                     <div>
-                                        <p style='text-align:center;' class="bigger-text">Custo </p>
-                                        <p style='text-align:center;' class="bigger-text"><b>114000</b> milhas</p>
+                                        <p style='text-align:center;' class="bigger-text">Total necessário </p>
+                                        <p style='text-align:center;' class="bigger-text"><b><?php echo $row['valor']?></b> milhas</p>
+                                    </div>
+                                    <div>
+                                        <p class="special-text"> Reserve agora e ganhe +500 bônus </p>
+                                    </div>
+                                    <div class="button-wrapper">
+                                        <button type='button' class='tabs-button' onclick="mostrarConfirmacao()">Reservar</button>
+                                        <!-- <button type='button' class='tabs-button' onClick="">Diminuir</button> -->
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        
+                    <?php  } ?>
+
+                    <?php     
+                            while($row = mysqli_fetch_array($hoteis, MYSQLI_ASSOC)){
+                            
+                        ?>
+                        <div class="box">
+                        <!-- <p class="title">Hoteis</p> -->
+                            <div class="wrapper">
+                                <div class="esquerda">
+                                    <div>
+                                        <p class="text"> <?php echo $row['nome']; ?> </p>
+                                        <p class="text"> <?php echo $destino; ?> </p>
+                                        
+                                    </div>
+                                    <div>
+                                        <p class="text"> <?php 10/01/2022 ?> </p>
+                                    </div>
+                                </div>
+                                <div class="direita">
+                                    <div>
+                                        <p style='text-align:center;' class="bigger-text">Total necessário</p>
+                                        <p style='text-align:center;' class="bigger-text"><b> <?php echo $row['valor']; ?></b> milhas</p>
+                                    </div>
+                                    <div>
+                                        <p class="special-text"> Continue juntando </p>
+                                    </div>
+                                    <div class="button-wrapper">
+                                        <button type='button' class='tabs-button'>Reservar</button>
+                                        <!-- <button type='button' class='tabs-button' onClick="">Diminuir</button> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
         
                     </div>
-                    
+                                
                 
                     
                 </div> 
